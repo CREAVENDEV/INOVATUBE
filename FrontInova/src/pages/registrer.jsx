23089b3a962d76ed6bd329e529f8ea7399460dc7
Omar Crespo <omar.crespo@seconad.com>
2025-06-03 21:30:09 -0600

    NO DEPLOY: Se agrego confirmación de contraseña

--- FrontInova/src/pages/registrer.jsx
+++ FrontInova/src/pages/registrer.jsx
@@ -4,7 +4,9 @@ import { useForm }  from "react-hook-form"
 import {registerrequest} from '../api/auth'
 
 export function Registrer(){
-    const {register,handleSubmit} = useForm()
+    const {register,handleSubmit,watch,formState:{errors}} = useForm()
+    const password = watch("password"); /*Se usa para declarar el widget a evaluar */
+
     return (
       <div className="bg-zinc-800 max-w-md p-10 rounded-md mt-40 mx-130">
         <h1>Registrece por favor</h1>
@@ -12,9 +14,9 @@ export function Registrer(){
 
         <form
           onSubmit={handleSubmit(async (values) => {
-            console.log(values);
             const res = await registerrequest(values);
-            console.log(res);
+            alert("¡Registro exitoso!");
+
           })}
         >
           <input
@@ -26,10 +28,24 @@ export function Registrer(){
 
           <input
             type="password"
-            {...register("password", { required: true })}
+            {...register("password", {
+              required: "La contraseña es obligatoria",
+            })}
             className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
             placeholder="Pass"
           />
+          {errors.password && <p>{errors.password.message}</p>}
+
+          <input
+            type="password"
+            {...register("confirmPassword", {
+              required: "Debes confirmar la contraseña",
+              validate: (value) => value === password || "Las Contraseñas no coinciden"
+            })}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+            placeholder="Confirm Pass"
+          />
+          {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
 
           <input
             type="email"
@@ -45,11 +61,7 @@ export function Registrer(){
             Registrar
           </button>
         </form>
-        <Link
-          
-          to="/"
-        >Regresar
-        </Link>
+        <Link to="/">Regresar</Link>
       </div>
     );
 }

c1ed9a898057df8c374dc40ee6eb324251cd7922
Omar Crespo <omar.crespo@seconad.com>
2025-06-09 13:55:32 -0600

    DEploy back 13

--- InnovaTube/settings.py
+++ InnovaTube/settings.py
@@ -80,10 +80,12 @@ WSGI_APPLICATION = 'InnovaTube.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+import dj_database_url
+DATABASE_URL='DATABASE_URL=postgresql://backendbdd_user:xWTCDjfqWOqpfSzF8PKHuAveVr9v6uSP@dpg-d13hgcm3jp1c73bshuqg-a/backendbdd'
 
 DATABASES = {
      'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL=postgres:backendbdd_user:xWTCDjfqWOqpfSzF8PKHuAveVr9v6uSP@dpg-d13hgcm3jp1c73bshuqg-a:5432/db'),
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True
     )

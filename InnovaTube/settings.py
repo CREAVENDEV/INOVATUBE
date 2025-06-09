bb33b38dec66ccbc0b18e8d54c359720b2e5876b
Omar Crespo <omar.crespo@seconad.com>
2025-06-09 11:44:24 -0600

    Deploy Back8

--- InnovaTube/settings.py
+++ InnovaTube/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=i_&r46k6b*$&pmvp8v^p8vmxg*b%m80jehuh(8%v(4ousbb*y' 
+SECRET_KEY = 'django-insecure-=i_&r46k6b*$&pmvp8v^p8vmxg*b%m80jehuh(8%v(4ousbb*y' ## 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
